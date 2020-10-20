617b0e0b57c228896573c9b5de934c85faa98c13
mihaidld <mihaidoldur@gmail.com>
2020-10-20 13:55:33 +0200

    added 2 files for token and ICO

---
+++ FirstIco.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "./FirstErc20.sol";
+contract FirstIco {
+    // Declare a FirstErc20 contract
+    FirstErc20 public token;
+
+    // The price of 1 unit of our token in wei;
+    uint256 public _price;
+
+    // Address of token seller
+    address payable private _seller;
+
+    constructor(uint256 price, address payable seller, address erc20Address ) public {
+        _price = price;
+        _seller = seller;
+        //Token is deployed at 0xE82d4ddbdB0530419f51563eAAd7A7C512C4b1d5
+        token = FirstErc20(erc20Address);
+    }
+
+    // receive external function payable which handles automatically the receipt of ether inside the contract from user, is called when the amount is sent by the user
+    receive() external payable {
+        require(msg.value >= 0, "ICO: Price is not 0 ether");
+        uint nbTokens = msg.value / _price;
+        token.transferFrom(_seller, msg.sender, nbTokens);
+        _seller.transfer(msg.value);
+    }
+
+    function buy(uint256 nbTokens) public payable returns(bool){
+        require(msg.value >= 0, "ICO: Price is not 0 ether");
+        require(nbTokens * _price <= msg.value, "ICO: Not enough Ether for purchase");
+        uint256 _realPrice = nbTokens * _price;
+        uint256 _remaining = msg.value - _realPrice;
+        token.transferFrom(_seller, msg.sender, nbTokens);
+        _seller.transfer(_realPrice);
+        if(_remaining > 0) {
+            msg.sender.transfer(_remaining);
+        }
+        return true;
+    }
+}
